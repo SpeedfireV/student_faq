db8f9a41e37d620958a3dd3e619ec4d1ec7ba9e2
grzan <84906236+SpeedfireV@users.noreply.github.com>
2025-04-09 18:35:34 +0200

    auth funcs

---
+++ lib/models/question/question_model.dart
@@ -0,0 +1,20 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:student_faq/models/answer/answer_model.dart';
+
+part 'question_model.freezed.dart';
+
+part 'question_model.g.dart';
+
+@freezed
+class Question with _$Question {
+  const factory Question({
+    required String questionName,
+    required List<Answer> answers,
+    required DateTime dateTime,
+    @Default(false) bool answered,
+    String? questionId,
+  }) = _Question;
+
+  factory Question.fromJson(Map<String, Object?> json) =>
+      _$QuestionFromJson(json);
+}
---
+++ lib/services/auth_service.dart
@@ -0,0 +1,48 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+
+class AuthService {
+  static Future<UserCredential?> logIn(
+      String emailAddress, String password) async {
+    debugPrint("working");
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailAddress,
+        password: password,
+      );
+      return credential;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+      throw e;
+    } catch (e) {
+      print(e);
+    }
+    return null;
+  }
+
+  static Future<UserCredential?> register(
+      String emailAddress, String password) async {
+    try {
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailAddress,
+        password: password,
+      );
+      return credential;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+      print(e);
+    } catch (e) {
+      print(e);
+    }
+    return null;
+  }
+}
