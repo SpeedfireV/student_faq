8db9dc08a255e8d4209102be23033c11a7693bcb
grzan <84906236+SpeedfireV@users.noreply.github.com>
2025-05-17 11:02:49 +0200

    working on firebase funcs

--- student_faq_functions/functions/main.py
+++ student_faq_functions/functions/main.py
@@ -54,6 +54,53 @@ def create_group(req: https_fn.CallableRequest):
     except Exception as e:
         raise https_fn.HttpsError(code=https_fn.FunctionsErrorCode.INTERNAL, message="Error updating user groups!")
 
+@https_fn.on_call()
+def delete_group(req: https_fn.CallableRequest):
+    db = firestore.client()
+    try:
+        user_uid = req.auth.uid
+    except Exception as e:
+        raise https_fn.HttpsError(code=https_fn.FunctionsErrorCode.UNAUTHENTICATED, message="User not logged in!")
+
+    meeting_uid = uuid.uuid4()
+
+    # Create group
+    try:
+        doc = {
+            "name": req.data["name"],
+            "description": req.data["description"] if req.data.get("description") else "",
+            "black_list": req.data["black_list"] if req.data.get("black_list") else [],
+            "description": req.data["description"] if req.data.get("description") else "",
+            "members": {
+                user_uid: "SuperModerator"
+            },
+            "sessions": req.data["sessions"] if req.data.get("sessions") else []
+        }
+    except Exception as e:
+        raise https_fn.HttpsError(code=https_fn.FunctionsErrorCode.INVALID_ARGUMENT, message="Invalid request data!")
+
+    try:
+        db.collection('groups').add(doc, str(meeting_uid))
+    except Exception as e:
+        raise https_fn.HttpsError(code=https_fn.FunctionsErrorCode.INTERNAL, message="Error creating group!")
+
+    # Attach group to user
+    try:
+        user_groups_snapshot: list[str] = db.collection('users').document(user_uid).get(["groups"]).to_dict()["groups"]
+    except Exception as e:
+        if e is TypeError:
+            raise https_fn.HttpsError(code=https_fn.FunctionsErrorCode.INVALID_ARGUMENT, message="Groups type doesn't match list[str]!")
+        raise https_fn.HttpsError(code=https_fn.FunctionsErrorCode.INTERNAL, message="Error getting user groups!")
+    user_groups_snapshot.append(str(meeting_uid))
+    try:
+        db.collection('users').document(user_uid).update({"groups": user_groups_snapshot})
+    except Exception as e:
+        raise https_fn.HttpsError(code=https_fn.FunctionsErrorCode.INTERNAL, message="Error updating user groups!")
+
+
+
+
+
 @identity_fn.before_user_created()
 def on_user_created(before_user: identity_fn.AuthBlockingEvent) -> identity_fn.BeforeCreateResponse | None:
     def get_random_name() -> str:
