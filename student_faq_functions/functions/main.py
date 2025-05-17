33583ac340168f0289783e6a9750f3d7a0b1f2eb
SpeedfireV <grzegorz.niespodzianek@gmail.com>
2025-05-17 12:29:50 +0200

    delete firebase func added

--- student_faq_functions/functions/main.py
+++ student_faq_functions/functions/main.py
@@ -54,51 +54,39 @@ def create_group(req: https_fn.CallableRequest):
     except Exception as e:
         raise https_fn.HttpsError(code=https_fn.FunctionsErrorCode.INTERNAL, message="Error updating user groups!")
 
+
 @https_fn.on_call()
 def delete_group(req: https_fn.CallableRequest):
-    db = firestore.client()
+    db= firestore.client()
     try:
         user_uid = req.auth.uid
     except Exception as e:
         raise https_fn.HttpsError(code=https_fn.FunctionsErrorCode.UNAUTHENTICATED, message="User not logged in!")
 
-    meeting_uid = uuid.uuid4()
-
-    # Create group
     try:
-        doc = {
-            "name": req.data["name"],
-            "description": req.data["description"] if req.data.get("description") else "",
-            "black_list": req.data["black_list"] if req.data.get("black_list") else [],
-            "description": req.data["description"] if req.data.get("description") else "",
-            "members": {
-                user_uid: "SuperModerator"
-            },
-            "sessions": req.data["sessions"] if req.data.get("sessions") else []
-        }
+        group_uid: str = req.data["group_uid"]
     except Exception as e:
-        raise https_fn.HttpsError(code=https_fn.FunctionsErrorCode.INVALID_ARGUMENT, message="Invalid request data!")
+        raise https_fn.HttpsError(code=https_fn.FunctionsErrorCode.INVALID_ARGUMENT, message="Invalid request data! User has to provide group_uid.")
 
-    try:
-        db.collection('groups').add(doc, str(meeting_uid))
-    except Exception as e:
-        raise https_fn.HttpsError(code=https_fn.FunctionsErrorCode.INTERNAL, message="Error creating group!")
+    doc = db.collection('groups').document(group_uid)
+    members: dict[str, str] = doc.get(["members"]).to_dict()["members"]
+    if members.get(user_uid) is not None and (members[user_uid] == "SuperModerator" or members[user_uid] == "Moderator"):
+        # Delete user-group relationship
+        try:
 
-    # Attach group to user
-    try:
-        user_groups_snapshot: list[str] = db.collection('users').document(user_uid).get(["groups"]).to_dict()["groups"]
-    except Exception as e:
-        if e is TypeError:
-            raise https_fn.HttpsError(code=https_fn.FunctionsErrorCode.INVALID_ARGUMENT, message="Groups type doesn't match list[str]!")
-        raise https_fn.HttpsError(code=https_fn.FunctionsErrorCode.INTERNAL, message="Error getting user groups!")
-    user_groups_snapshot.append(str(meeting_uid))
-    try:
-        db.collection('users').document(user_uid).update({"groups": user_groups_snapshot})
-    except Exception as e:
-        raise https_fn.HttpsError(code=https_fn.FunctionsErrorCode.INTERNAL, message="Error updating user groups!")
-
-
-
+            for user_uid in members.keys():
+                user_doc = db.collection('users').document(user_uid)
+                updated_groups = user_doc.get(["groups"]).to_dict()["groups"].remove(str(group_uid))
+                user_doc.update({"groups": updated_groups})
+        except Exception as e:
+            raise https_fn.HttpsError(code=https_fn.FunctionsErrorCode.INTERNAL, message="Error deleting user-group relationship!")
+        # Delete group
+        try:
+            doc.delete()
+        except Exception as e:
+            raise https_fn.HttpsError(code=https_fn.FunctionsErrorCode.INTERNAL, message="Error deleting group!")
+    else:
+        raise https_fn.HttpsError(code=https_fn.FunctionsErrorCode.PERMISSION_DENIED, message="User is not a member of the group!")
 
 
 @identity_fn.before_user_created()
